29ef386efc43922447f3357f3ef92f12fbfdcdf2
cunhasb CUNHA <cunhasb@aol.com>
2017-10-02 17:21:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-
-    
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
