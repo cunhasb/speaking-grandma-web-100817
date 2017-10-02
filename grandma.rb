06863695bc065af50595eff28e296f2c9ef4040e
cunhasb CUNHA <cunhasb@aol.com>
2017-10-02 17:27:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    if phrase == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
+    else
+      "NO, NOT SINCE 1938!"
+    end
   else
     "HUH?! SPEAK UP, SONNY!"
   end
